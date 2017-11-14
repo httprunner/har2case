dfffd67cf011f735cbeea5a6f2b0338b4ca9927a
debugtalk <mail@debugtalk.com>
2017-11-14 17:54:25 +0800

    fix TypeError

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
