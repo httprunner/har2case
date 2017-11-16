fccd31ea1c12cc060fdef9d80a9dda4cddca51b1
debugtalk <mail@debugtalk.com>
2017-11-16 13:48:48 +0800

    relocate code and tests

--- tests/test_har.py
+++ tests/test_har.py
@@ -1,55 +1,10 @@
-import json
 import os
-import unittest
 
-from har2case.core import HarParser, load_har_log_entries, x_www_form_urlencoded
+from har2case.core import HarParser
+from tests.test_utils import TestUtils
 
 
-class TestHar(unittest.TestCase):
-
-    @staticmethod
-    def create_har_file(file_name, content):
-        file_path = os.path.join(
-            os.path.dirname(__file__), "data", "{}.har".format(file_name))
-        with open(file_path, "w") as f:
-            f.write(json.dumps(content))
-
-        return file_path
-
-    @classmethod
-    def setUpClass(cls):
-        cls.har_path = os.path.join(
-            os.path.dirname(__file__), "data", "demo.har")
-        cls.empty_file_path = TestHar.create_har_file(file_name="empty", content="")
-        cls.empty_json_file_path = TestHar.create_har_file(file_name="empty_json", content={})
-
-    @classmethod
-    def tearDownClass(cls):
-        os.remove(cls.empty_file_path)
-        os.remove(cls.empty_json_file_path)
-
-    def test_load_har_log_entries(self):
-        log_entries = load_har_log_entries(self.har_path)
-        self.assertIsInstance(log_entries, list)
-        self.assertIn("request", log_entries[0])
-        self.assertIn("response", log_entries[0])
-
-    def test_load_har_log_key_error(self):
-        with self.assertRaises(SystemExit):
-            load_har_log_entries(self.empty_json_file_path)
-
-    def test_load_har_log_empty_error(self):
-        with self.assertRaises(SystemExit):
-            load_har_log_entries(self.empty_file_path)
-
-    def test_x_www_form_urlencoded(self):
-        origin_dict = {"a":1, "b": "2"}
-        self.assertEqual(
-            x_www_form_urlencoded(origin_dict),
-            "a=1&b=2"
-        )
-
-class TestHarParser(TestHar):
+class TestUtilsParser(TestUtils):
 
     def setUp(self):
         self.har_parser = HarParser(self.har_path)
---
+++ tests/test_utils.py
@@ -0,0 +1,50 @@
+import json
+import os
+import unittest
+
+from har2case import utils
+
+
+class TestUtils(unittest.TestCase):
+
+    @staticmethod
+    def create_har_file(file_name, content):
+        file_path = os.path.join(
+            os.path.dirname(__file__), "data", "{}.har".format(file_name))
+        with open(file_path, "w") as f:
+            f.write(json.dumps(content))
+
+        return file_path
+
+    @classmethod
+    def setUpClass(cls):
+        cls.har_path = os.path.join(
+            os.path.dirname(__file__), "data", "demo.har")
+        cls.empty_file_path = TestUtils.create_har_file(file_name="empty", content="")
+        cls.empty_json_file_path = TestUtils.create_har_file(file_name="empty_json", content={})
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.empty_file_path)
+        os.remove(cls.empty_json_file_path)
+
+    def test_load_har_log_entries(self):
+        log_entries = utils.load_har_log_entries(self.har_path)
+        self.assertIsInstance(log_entries, list)
+        self.assertIn("request", log_entries[0])
+        self.assertIn("response", log_entries[0])
+
+    def test_load_har_log_key_error(self):
+        with self.assertRaises(SystemExit):
+            utils.load_har_log_entries(self.empty_json_file_path)
+
+    def test_load_har_log_empty_error(self):
+        with self.assertRaises(SystemExit):
+            utils.load_har_log_entries(self.empty_file_path)
+
+    def test_x_www_form_urlencoded(self):
+        origin_dict = {"a":1, "b": "2"}
+        self.assertEqual(
+            utils.x_www_form_urlencoded(origin_dict),
+            "a=1&b=2"
+        )
